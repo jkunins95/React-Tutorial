c65e124e070e562165213ddeb036857bfaf80aad
+ <jkunins95@gmail.com>
2023-10-23 15:38:26 -0400

    deleted null

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -11,8 +11,8 @@ function ListGroup() {
   return (
     <>
       <h1>List</h1>
-      {/* Ternary Operator */}
-      {items.length === 0 ? <p>No item found</p> : null}
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
