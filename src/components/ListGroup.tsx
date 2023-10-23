4a78a9a7967c146889b2f9592c5ec8abe6f5e9ce
+ <jkunins95@gmail.com>
2023-10-23 15:36:11 -0400

    added ternary operator

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -8,18 +8,11 @@ function ListGroup() {
   ];
   items = [];
 
-  // Conditional Statement BUT with duplication - not considered good practice
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No Item Found</p>
-      </>
-    )
-
   return (
     <>
       <h1>List</h1>
+      {/* Ternary Operator */}
+      {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
