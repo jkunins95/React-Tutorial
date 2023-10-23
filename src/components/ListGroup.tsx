ada12f4a6ecf4a04adc1567055939f51dc4efa9c
+ <jkunins95@gmail.com>
2023-10-23 15:30:47 -0400

    conditional rendering part 1

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,22 @@
 function ListGroup() {
-  const items = [
+  let items = [
     'New York',
     'San Francisco',
     'Tokyo',
     'London',
     'Paris',
   ];
+  items = [];
 
-  
+  // Conditional Statement BUT with duplication - not considered good practice
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No Item Found</p>
+      </>
+    )
+
   return (
     <>
       <h1>List</h1>
